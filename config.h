bf135b5a18ba20d53f75148590da1f1bf3c9776e
mYmNeo <mymneo@163.com>
2015-03-16 17:26:37 +0800

    add new solution

--- config.h
+++ config.h
@@ -43,3 +43,10 @@ struct TreeLinkNode
     TreeLinkNode *next;
 };
 
+struct Interval
+{
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+};
